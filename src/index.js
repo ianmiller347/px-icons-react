a7ab149d70c8e27cedafbbce34b9675f8473a371
Ian J. Miller <Email>
2018-05-07 15:41:01 -0500

    update to only use svg, no wrapper

--- src/index.js
+++ src/index.js
@@ -5,18 +5,16 @@ import icons from './icons.json';
 
 // this is basically a clone of "Feather Icons React", edited for Predix UI Icons.
 // main difference is that this lib uses full svg markup
-// until viewBox attribute can be set for each one, or it is parsed,
-// will need to have this weird span wrapper :\
+// so a regex is called to pull the viewBox attribute and use it.
+// it defaults to 24x24, but px icons are not based on that grid.
+// until all px icons are based on the same grid, 
+// or another attribute is passed to describe which grid to use,
+// this viewBox attribute needs to be passed accordingly.
 
 class PxIcon extends Component {
   createMarkup(markup, size) {
-    // sort of a hacky mess to set the width height to what user passes.
-    // can be updated when the icons.json does not need non-square viewBox.
-    // also change this component to accept width and height for non-square icons.
-    const widthHeightAttr = `width="${size}" height="${size}"`;
-    const editedMarkup = markup.replace(/viewBox="([^"]+)"/, `viewBox="$1" ${widthHeightAttr}`)
     // sanitize markup first
-    const sanitizedMarkup = DOMPurify.sanitize(editedMarkup);
+    const sanitizedMarkup = DOMPurify.sanitize(markup);
     // now do the weird thing for dangerouslySetInnerHTML
     return {__html: sanitizedMarkup};
   }
@@ -29,17 +27,21 @@ class PxIcon extends Component {
     // this way, user can do both px-util:close and util:close
     const iconNoPx = icon.replace('px-', '');
     const iconMarkup = icons[iconNoPx];
+    const insides = iconMarkup
+      .replace(/<svg ([^>]+)>/, '')
+      .replace('</svg>', '');
+    const viewBoxAttr = match(/viewBox="([^"]+)"/)[1] || '0 0 24 24';
 
-    // just wrapping in a span for this early version.
-    // currently, icons.json has svg in it and needs that for proper viewBox
-    // size attribute won't work properly, but use CSS for that.
     if (iconMarkup) {
       return (
-        <span
+        <svg
+          width={size}
+          height={size}
+          viewBox={viewBoxAttr}
           className={`px-icon icon-${classFriendlyText} ${className}`}
-          {...otherProps}
-          dangerouslySetInnerHTML={this.createMarkup(iconMarkup, size)} 
-        />
+          {...otherProps}>
+          <g dangerouslySetInnerHTML={this.createMarkup(insides, size)} />
+        </svg>
       );
     }
     return null;
